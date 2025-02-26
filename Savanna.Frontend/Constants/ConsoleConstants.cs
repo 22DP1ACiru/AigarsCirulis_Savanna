09fe2ff12053e1a28b7271b135c0066d1f396270
cirulis-aigars <11dpacirulis@rvt.lv>
2025-02-26 11:42:37 +0200

    Define Frontend console constants, add project reference to Backend

---
+++ Savanna.Frontend/Constants/ConsoleConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Savanna.Frontend.Constants
+{
+    public static class ConsoleConstants
+    {
+        public const char GridBorderHorizontal = '-';
+        public const char GridBorderVertical = '|';
+        public const char GridBorderCorner = '+';
+        public const string GameTitle = "Savanna Wildlife Simulation";
+        public const string ControlsInfo = "Controls: [A] Add Antelope, [L] Add Lion, [Esc] Exit";
+        public const int InfoPanelWidth = 40;
+    }
+}
