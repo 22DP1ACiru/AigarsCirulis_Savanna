637e1b682cfdfae664d40d236a7756d5fc065a6b
cirulis-aigars <11dpacirulis@rvt.lv>
2025-02-26 11:21:15 +0200

    Define backend functionality constants

--- Savanna.Backend/Constants/AnimalConstants.cs
+++ Savanna.Backend/Constants/AnimalConstants.cs
@@ -3,14 +3,15 @@
     public static class AnimalConstants
     {
         // Shared constants
-        public const int DefaultVisionRange = 3;
-        public const int PassiveVisionRange = 2;
+        public const int DefaultVisionRange = 4;
 
         // Antelope constants
+        public const int AntelopeVisionRange = 6;
         public const int AntelopeMovementSpeed = 1;
         public const char AntelopeSymbol = 'A';
 
         // Lion constants
+        public const int LionVisionRange = 5;
         public const int LionMovementSpeed = 2;
         public const char LionSymbol = 'L';
         public const int LionDigestionTime = 2;
