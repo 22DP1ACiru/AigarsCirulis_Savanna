861a1e99c6ccef9c1ac457c411f1ae0f16ac27f9
cirulis-aigars <11dpacirulis@rvt.lv>
2025-03-26 15:40:46 +0200

    Chore: add mock animal factory to engine tests to build correctly

--- Savanna.Tests/GameEngineTests.cs
+++ Savanna.Tests/GameEngineTests.cs
@@ -2,17 +2,28 @@
 using Savanna.Backend.Models;
 using Savanna.Backend;
 using Savanna.Backend.Constants;
+using Savanna.Backend.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Savanna.Tests
 {
     [TestClass]
     public class GameEngineTests
     {
+        private Mock<IAnimalFactory> _mockAnimalFactory;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockAnimalFactory = new Mock<IAnimalFactory>();
+        }
+
         [TestMethod]
         public void AddAnimal_ShouldAddAnimalToGameGrid()
         {
             // Arrange
-            var gameEngine = new GameEngine();
+            var gameEngine = new GameEngine(_mockAnimalFactory.Object);
             gameEngine.Initialize();
             var antelope = new Antelope(new Position(0, 0));
 
@@ -28,7 +39,7 @@ namespace Savanna.Tests
         public void Update_ShouldRemoveDeadAnimals()
         {
             // Arrange
-            var gameEngine = new GameEngine();
+            var gameEngine = new GameEngine(_mockAnimalFactory.Object);
             gameEngine.Initialize();
             var antelope = new Antelope(new Position(0, 0));
             gameEngine.AddAnimal(antelope);
