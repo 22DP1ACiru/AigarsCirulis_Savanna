3aac9e7cd999eb6025d8c3e7dfd15f3ccadb2391
cirulis-aigars <11dpacirulis@rvt.lv>
2025-03-27 10:30:39 +0200

    Update homepage and navbar to include links to game dashboard

--- Savanna.Web/Pages/Index.cshtml.cs
+++ Savanna.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Savanna.Data.Entities;
 
 namespace Savanna.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public bool IsUserLoggedIn { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, SignInManager<ApplicationUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public void OnGet()
         {
-
+            IsUserLoggedIn = _signInManager.IsSignedIn(User);
         }
     }
 }
